556828964470e3f2520e352c8008f078d305b214
Hardique Dasore <37050252+hardiquedasore@users.noreply.github.com>
2023-08-13 00:18:28 +1000

    Update Tree.js

--- src/app/pages/Tree.js
+++ src/app/pages/Tree.js
@@ -10,7 +10,7 @@ const Tree = () => {
     <div className="flex flex-col items-center justify-center mt-10 mb-10 min-h-screen w-full">
       <Image className="rounded-3xl" src={constants.logo} width={96} height={96} />
       <h1 className="text-5xl pt-4 font-bold pb-2">{constants.brand}</h1>
-      <p className="text-lg  mb-1" >{constants.description}</p>
+      <p className="text-lg max-w-sm text-justify mb-1" >{constants.description}</p>
       <div className="flex flex-col items-center w-full max-w-xs">
         {links.map((link, index) => (
           <LinkButtons key={index} title={link.title} url={link.url} borderColor={link.borderColor} bgImage={link.bgImage} logo={link.icon} textColor={link.textColor} buttonColor={link.buttonColor} />
